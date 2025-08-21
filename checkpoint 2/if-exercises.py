030cdd73e60de5ca77657b9885e0776fc961d941
Lu <navarreteluisa71@gmail.com>
2025-08-21 20:20:03 +0000

    j

---
+++ checkpoint 2/if-exercises.py
@@ -0,0 +1,8 @@
+def main():
+    x = int(input("Type any number"))
+    print("The result is", x)
+
+    if x < -x:
+        print(x * -1)
+
+main()
---
+++ checkpoint 2/if-exercises2.py
@@ -0,0 +1,6 @@
+def main():
+    number = int(input("Type two numbers"))
+    number = int(input("Choose any operation"))
+    cheeck_operation(number)
+
+    
