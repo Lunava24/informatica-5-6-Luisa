410208b0bdb5c77b7ac3bd5213afafeb7ddcb9e0
Lu <navarreteluisa71@gmail.com>
2025-08-26 18:07:27 +0000

    g

---
+++ checkpoint 3/goblin.py
@@ -0,0 +1,15 @@
+import random 
+print("Welcome to the Goblin Game")
+print("the best game ever")
+player_name = input("Write your name: ")
+print(player_name, "can you findthe goblin?")
+print("|_||_||_||_||_|")
+goblin_position = random.randint (1, 5)
+Keep_trying = True
+while Keep_trying:
+    guessed_position = int(input("can you guess where the goblin is"))
+
+    if goblin_position == guessed_position:
+        print("🟢Well done!! you find the goblin🟢")
+        Keep_trying = False
+    else print("🔴NO, The goblin still hide somewhere else🔴")
