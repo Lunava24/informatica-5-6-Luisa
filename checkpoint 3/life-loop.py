74564caf43117da9704fbee1acc3201b8710fec8
Lu <navarreteluisa71@gmail.com>
2025-08-29 17:29:59 +0000

    g

---
+++ checkpoint 3/life-loop.py
@@ -0,0 +1,19 @@
+import time
+
+from attr import _Nothing
+
+def main():
+    word = input("What are you doing?")
+    time.sleep(0.5)
+    print("I'm ")
+
+if ord(_Nothing):
+    print("Read somthing or do exercise")
+
+
+
+#print("Starting program...")
+#time.sleep(1)  # Pause execution for 1 second
+#print("Program continues after 1 second.")
+#time.sleep(0.5) # Pause execution for 0.5 seconds
+#print("Program continues after another 0.5 seconds.")
