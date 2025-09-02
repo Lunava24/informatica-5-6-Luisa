f2947864ecc24b95f42365c551e0ef9088e585cf
Lu <navarreteluisa71@gmail.com>
2025-09-02 16:39:59 +0000

    a

---
+++ Checkpoint 4/cipher.py
@@ -0,0 +1,16 @@
+def main():
+    message = input("Type a message").lower()
+    encode_message(message)
+
+def encode_message(text): 
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    cipher = "zyxwvutsrqponmlkjihgfedcba"
+    new_message = ""
+    i = 0
+
+    while i < len(text):
+        char = text[i]
+
+
+
+main()
