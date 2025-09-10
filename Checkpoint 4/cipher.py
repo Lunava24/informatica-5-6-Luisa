deff2fbff3e14401f8d89e602e75d6acb6e05c76
Lu <navarreteluisa71@gmail.com>
2025-09-10 20:20:13 +0000

    g

--- Checkpoint 4/cipher.py
+++ Checkpoint 4/cipher.py
@@ -1,19 +1,19 @@
-def main(): #
-    message = input("Type a message: ").lower() #This is to start the function
+def main():  #Starting a new function
+    message = input("Type a message: ").lower() #Get the message 
     encode_message(message) # calling the function
 
 
 def encode_message(text): #Starting a new function
     alphabet = "abcdefghijklmnopqrstuvwxyz" #This is the alphabet variable 
     cipher = "zyxwvutsrqponmlkjihgfedcba" #Transposing all the letters in the alphabet such that the resulting alphabet is backwards
-    new_message = "" #
-    i = 0 #The first letter is iqual to 0
+    new_message = "" # create an empty variable to add the cipher
+    i = 0 # Counting of variable index
 
-    while i < len(text): #
-        char = text[i] #
+    while i < len(text): # Count aslong as variable lengh
+        char = text[i] # Get the character from the variable
 
-        if char in alphabet: #
-            cipher_index = alphabet.find(char) #
+        if char in alphabet: # checks if char is in the alphabet
+            cipher_index = alphabet.find(char) # 
             new_message += cipher[cipher_index] #
         else: #
             new_message += char #
---
+++ Checkpoint 5/radio-playlist.py
@@ -0,0 +1,12 @@
+weekly_playlist = ["Blinding Lights", "Levitating", "As It Was", "Heat Waves", "Good 4 u"]
+print(weekly_playlist)
+
+weekly_playlist.append("Drivers License")
+weekly_playlist.insert(0,"Bohemian Rhapsody")
+weekly_playlist.remove("Good 4 u")
+print(weekly_playlist.index("Levitating"))
+weekly_playlist.count("As it was")
+weekly_playlist.reverse()
+print(weekly_playlist)
+weekly_playlist.sort()
+print(len(weekly_playlist))
