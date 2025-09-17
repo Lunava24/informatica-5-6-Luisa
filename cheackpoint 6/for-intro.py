8874ead27446e00bf9d355c479621132be613cc1
Lu <navarreteluisa71@gmail.com>
2025-09-17 20:21:18 +0000

    e

---
+++ cheackpoint 6/for-intro.py
@@ -0,0 +1,15 @@
+doctrine = ["Faith", "repentence", "Baptism", "Holy Ghosth", "Endure to the end"]
+
+# i = 0
+# while 1 < len(doctrine):
+#     print(doctrine[1])
+#     1 += 1
+
+for principle in doctrine:
+    print(principle)
+
+school_objective = ["Deepem conversation to Jesus Christ and His restore Gospel", "Qualify for the blessings of the temple", "Prepare for missionary and service Church", "Build the Kingdom of God"]
+
+for obtective in school_objective:
+    print(obtective)
+    print("")
