85261aed39b033e73d2d2ffa573b518b4986598a
Lu <navarreteluisa71@gmail.com>
2025-09-18 20:19:44 +0000

    a

--- cheackpoint 6/file-io.py
+++ cheackpoint 6/file-io.py
@@ -1,12 +1,22 @@
 #names = []
 
-#for i in range(3): #crea listas "range"
+#for i in range(3): #make lists "range"
     #names.append(input("What's your name? "))
 
 #for name in sorted(names):
     #print(f"Hello {name}")
 
-name = input("What's your name? ")
-file = open("names.txt", "a")
-file.write(f"{name}\n")
-file.close()
+# name = input("What's your name? ")
+# file = open("names.txt", "a")
+# file.write(f"{name}\n")
+# file.close()
+
+# with open("name.txt", "a") as file:  #(file is a variable)
+#     file.write(f"{input("What's your name? ")}")  #
+
+
+with open("names.txt", "r") as file:
+    lines = file.readlines() 
+
+for line in sorted(lines):
+    print(f"Hello {line.rsplit()}")
