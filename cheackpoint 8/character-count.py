71265ece097d196b56b8738cde9f2939b28f767e
Lu <navarreteluisa71@gmail.com>
2025-09-30 16:40:11 +0000

    D

---
+++ cheackpoint 8/character-count.py
@@ -0,0 +1,9 @@
+message = input("Send a message: ")
+dictionary = {}
+for character in message:
+    dictionary[character] = dictionary.get(character, 0) + 1
+print(dictionary)
+
+print(len(message))
+
+find = max(dictionary, )
