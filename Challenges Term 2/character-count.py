0ba70792af406ec74f595d8bb46b6aff9e132de1
Lu <navarreteluisa71@gmail.com>
2025-10-05 20:56:43 +0000

    s

--- Challenges Term 2/character-count.py
+++ Challenges Term 2/character-count.py
@@ -1,4 +1,12 @@
 message = input("Send a message: ")
+filename = "message.txt"
+
+with open(filename, 'w') as file:
+    file.write(message)
+
+with open(filename, "r") as file:
+    message = file.readline().strip()
+
 dictionary = {}
 for character in message:
     dictionary[character] = dictionary.get(character, 0) + 1
@@ -8,7 +16,3 @@ print(len(message))
 
 find = max(dictionary, key=dictionary.get)
 print(f"The largest word is: {find}, It appears: {dictionary[find]} times") 
-
-#
-#
-#
