5fe9bfe23b1f05843b8b94ec5cde979eeff95494
Lu <navarreteluisa71@gmail.com>
2025-10-30 20:20:16 +0000

    J

---
+++ cheakpoint-10/calories.py
@@ -0,0 +1,24 @@
+protein_list = {
+    "Whole": "73",
+    "Almond milk": "30",
+    "Sour Cream, light": "16",
+    "Plain, low-fat": "77",
+    "Egg": "75",
+    "Egg white": "16",
+    "Cream Cheese": "17",
+    "American Pasteurized": "79",
+    "Peanuts, roasted": "166",
+    "Sunflower seeds, dry roasted": "165",
+    "Lentils, boiled": "115",
+    "Black beans, boiled": "113",
+    "Swai, baked": "89",
+    "Catfish, baked": "89",
+    "Broccoli": "7",
+    "Carrots, raw": "13",
+    "Watermelon": "11",
+    "Blueberries": "21",
+    "Quinoa, cooked": "56",
+    "Ranch": "73"
+}
+
+food = input("Choose a food: ")
