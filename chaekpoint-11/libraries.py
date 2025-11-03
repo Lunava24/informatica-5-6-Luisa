db40a904417b6657035c6d06120be8fff5470c4f
Lu <navarreteluisa71@gmail.com>
2025-11-03 19:39:52 +0000

    z

---
+++ chaekpoint-11/libraries.py
@@ -0,0 +1,23 @@
+# import random
+
+# coin = random.choice(["heads", "tails"])
+# print(coin)
+
+# number = random.randint(1,10)
+# print(number)
+
+# cards = ["Jack", "Queen", "King", "Ace"]
+# random.shuffle(cards)
+# for card in cards:
+#     print(card)
+
+# import statistics
+# print(statistics.mean([100,90]))
+
+import sys
+try:
+    print("Hello, my name is:", sys.argv[1])
+except IndexError:
+    # print("Too few arguments")
+    sys.exit()
+
