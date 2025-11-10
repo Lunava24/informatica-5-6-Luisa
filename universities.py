2dc71dcb9d4a7c3f25a0ad1b6ce99eb722e57fa9
Lu <navarreteluisa71@gmail.com>
2025-11-10 19:38:49 +0000

    n

---
+++ universities.py
@@ -0,0 +1,38 @@
+def main():
+    dictionary = {
+        "EAC": {
+            "Number of Majors": 140,
+            "Average semester cost": 0,
+            "Closest campus location": "Thatcher",
+            "Distance_km": 466,
+        },
+        "URN": {
+            "Number of Majors": 29,
+            "Average semester cost": 10000,
+            "Closest campus location": "Cuidad Juarez",
+            "Distance_km": 290,
+        },
+        "tec_Casas_Grandes": {
+            "Number of Majors": 8,
+            "Average semester cost": 3000,
+            "Closest campus location": "Casas Grandes",
+            "Distance_km": 27,
+        },
+        "BYU_Pathway": {
+            "Number of Majors": 30,
+            "Average semester cost": 30000,
+            "Closest campus location": "Online",
+            "Distance_km": 0,
+        },
+        "UACJ": {
+            "Number of Majors": 37,
+            "Average semester cost": 5000,
+            "Closest campus location": "Cuidad Juarez",
+            "Distance_km": 280,
+        }
+    }
+
+name_university = input("Select an university: ")
+print()
+
+main()
